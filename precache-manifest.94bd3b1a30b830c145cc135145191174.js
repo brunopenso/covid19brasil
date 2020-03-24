9e416e4ef56f65f52e3638656c39552be5ba2d7d
brunopenso <bruno.penso@gmail.com>
2020-03-24 01:10:59 +0000

    Deploying to master from  - 667a4fb91f74d317b19a814b11c1cd6438947942 🚀

---
+++ precache-manifest.94bd3b1a30b830c145cc135145191174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5e82c372bac0a34144a96fd745cfc8",
+    "url": "/covid19brasil/index.html"
+  },
+  {
+    "revision": "2ac69293bfc880250625",
+    "url": "/covid19brasil/static/css/main.565e4dd4.chunk.css"
+  },
+  {
+    "revision": "82c65200e12f96bcfb9d",
+    "url": "/covid19brasil/static/js/2.f5f3ef5a.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid19brasil/static/js/2.f5f3ef5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac69293bfc880250625",
+    "url": "/covid19brasil/static/js/main.cfe49930.chunk.js"
+  },
+  {
+    "revision": "c830817b5189131e4831",
+    "url": "/covid19brasil/static/js/runtime-main.4e27f796.js"
+  }
+]);
